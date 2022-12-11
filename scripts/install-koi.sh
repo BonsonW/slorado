eb6b5dcdb54e44ed799ce62fb10c89fed6f08088
hiruna72 <hiruna72@gmail.com>
2022-12-12 10:26:18 +1100

    move torch dir inside thirdparty dir

--- scripts/install-koi.sh
+++ scripts/install-koi.sh
@@ -3,9 +3,9 @@ set -e
 test -d koi && rm -r koi
 test -e koi.tar.gz && rm  koi.tar.gz
 wget https://nanoporetech.box.com/shared/static/qbasibmplodr2ixztz97v53vmkttxia1.gz -O koi.tar.gz
-mkdir koi
-tar xf koi.tar.gz -C koi
-mv koi/*/* koi/
+mkdir thirdparty/koi_lib
+tar xf koi.tar.gz -C thirdparty/koi_lib
+#mv koi/*/* koi/
 rm -r  koi.tar.gz
 
 
--- scripts/install-torch10.sh
+++ scripts/install-torch10.sh
@@ -6,6 +6,6 @@ test -e torch.zip && rm torch.zip
 test -d torch && rm -r torch
 mkdir torch/
 wget "https://download.pytorch.org/libtorch/cu102/libtorch-shared-with-deps-1.10.2%2Bcu102.zip" -O torch.zip
-unzip torch.zip -d torch/
+unzip torch.zip -d thirdparty/torch/
 rm torch.zip
 
