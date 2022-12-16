81022d97344efcea146e65bedc756d43e36ee57f
Bonson Wong <bonwon@genometech-gpgpu.garvan.unsw.edu.au>
2022-12-16 11:04:16 +1100

    cleanup files

--- scripts/install-torch10.sh
+++ scripts/install-torch10.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 test -e torch.zip && rm torch.zip
 test -d torch && rm -r torch
-mkdir torch/
+mkdir thirdparty/torch
 wget "https://download.pytorch.org/libtorch/cu102/libtorch-shared-with-deps-1.10.2%2Bcu102.zip" -O torch.zip
 unzip torch.zip -d thirdparty/torch/
 rm torch.zip
