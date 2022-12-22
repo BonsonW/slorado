4189feae391a0b75708e9deb5d5fce82479c9290
Bonson Wong <bonwon@genometech-gpgpu.garvan.unsw.edu.au>
2022-12-22 14:03:12 +1100

    updated decode dir

--- src/decode/GPUDecoder.h
+++ src/decode/GPUDecoder.h
@@ -1,22 +1,27 @@
 #pragma once
 
-#include <torch/torch.h>
 #include "Decoder.h"
 
+#include <torch/torch.h>
+
 class GPUDecoder : Decoder {
+public:
+    std::vector<DecodedChunk> beam_search(torch::Tensor scores,
+                                          int num_chunks,
+                                          DecoderOptions options) final;
+    constexpr static torch::ScalarType dtype = torch::kF16;
 
-    public:
-        std::vector<DecodedChunk> beam_search(torch::Tensor scores, int num_chunks, DecoderOptions options) final;
-        constexpr static torch::ScalarType dtype = torch::kF16;
+    // We split beam_search into two parts, the first one running on the GPU and the second
+    // one on the CPU. While the second part is running we can submit more commands to the GPU
+    // on another thread.
+    torch::Tensor gpu_part(torch::Tensor scores, int num_chunks, DecoderOptions options);
+    std::vector<DecodedChunk> cpu_part(torch::Tensor moves_sequence_qstring_cpu);
 
-    private:
-        torch::Tensor chunks;
-        torch::Tensor chunk_results;
-        torch::Tensor aux;
-        torch::Tensor path;
-        torch::Tensor moves;
-        torch::Tensor sequence;
-        torch::Tensor qstring;
-        bool initialized{false};
-        int get_cuda_device_id_from_device(const c10::Device& device);
+private:
+    torch::Tensor chunks;
+    torch::Tensor chunk_results;
+    torch::Tensor aux;
+    torch::Tensor path;
+    torch::Tensor moves_sequence_qstring;
+    bool initialized{false};
 };
