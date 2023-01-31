ff12de246805ddedc7ce50f7e0450643384f4b58
Bonson Wong <bonwon@genometech-gpgpu.garvan.unsw.edu.au>
2023-01-31 11:04:36 +1100

    fixed test script and makefile

--- scripts/run_dorados.sh
+++ scripts/run_dorados.sh
@@ -3,10 +3,10 @@
 MINIMAP2=../minimap2-2.24_x64-linux/minimap2 #path to minimpa2 executable if not added to $PATH
 REFERENC_GENOME="/genome/hg38noAlt.idx" #path to reference genome
 OUTPUT_DIR="logs"
-# POD5_FILE="/data/slow5-testdata/hg2_prom_lsk114_subsubsample/pod5"
-# BLOW5_FILE="/data/slow5-testdata/hg2_prom_lsk114_subsubsample/reads.blow5"
-POD5_FILE="/data/bonwon/slorado/test/pod5_dir"
-BLOW5_FILE="/data/bonwon/slorado/test/oneread_r10.blow5"
+POD5_FILE="/data/slow5-testdata/hg2_prom_lsk114_subsubsample/pod5"
+BLOW5_FILE="/data/slow5-testdata/hg2_prom_lsk114_subsubsample/reads.blow5"
+# POD5_FILE="/data/bonwon/slorado/test/pod5_dir"
+# BLOW5_FILE="/data/bonwon/slorado/test/oneread_r10.blow5"
 MODEL="/data/install/dorado-0.1.1/models/dna_r10.4.1_e8.2_400bps_fast@v4.0.0/"
 DEVICE="cuda:0,cuda:1"
 GPU_BATCHSIZE="200"
@@ -21,7 +21,7 @@ for var in "$@"
 do
     clean_fscache
     if [ "$var" = "slorado" ]; then
-        /usr/bin/time -v ./slorado basecaller -o ${OUTPUT_DIR}/slorado_calls.fastq -t 40 -x ${DEVICE} -B500M -C${GPU_BATCHSIZE} -K2000 -c10000 git${MODEL} ${BLOW5_FILE} 2>${OUTPUT_DIR}/slorado_log.txt
+        /usr/bin/time -v ./slorado basecaller -o ${OUTPUT_DIR}/slorado_calls.fastq -t 40 -x ${DEVICE} -B500M -C${GPU_BATCHSIZE} -K2000 -c10000 ${MODEL} ${BLOW5_FILE} 2>${OUTPUT_DIR}/slorado_log.txt
         get_accuracy "slorado_calls.fastq" "slorado_accuracy.txt"
         rm ${OUTPUT_DIR}/slorado_calls.fastq
     elif [ "$var" = "dorado_release" ]; then
