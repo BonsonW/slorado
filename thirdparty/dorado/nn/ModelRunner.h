eebe96a12dbc10932ab2ca10bbc1d8c8bdf59cb8
AmindaAmarasinghe <e17012@eng.pdn.ac.lk>
2023-06-05 22:16:15 +0530

    timestamps added for beam search emplace fixed v3

--- thirdparty/dorado/nn/ModelRunner.h
+++ thirdparty/dorado/nn/ModelRunner.h
@@ -14,7 +14,7 @@
 class ModelRunnerBase {
 public:
     virtual void accept_chunk(int chunk_idx, at::Tensor slice) = 0;
-    virtual std::vector<DecodedChunk> call_chunks(int num_chunks) = 0;
+    virtual std::vector<DecodedChunk> call_chunks(int num_chunks,timestamps_t* ts) = 0;
     virtual size_t model_stride() const = 0;
     virtual size_t chunk_size() const = 0;
 };
