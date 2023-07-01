137ef7072a8df737fb8ed899aa601fcd25b31b58
AchinthaSandakelum <achinthasandakelum45@gmail.com>
2023-07-01 10:21:39 +0530

    Addet check point on matmul

--- thirdparty/dorado/utils/cuda_utils.cpp
+++ thirdparty/dorado/utils/cuda_utils.cpp
@@ -20,6 +20,7 @@ extern "C" {
 #include <vector>
 
 void matmul_f16_cublas(torch::Tensor const &A, torch::Tensor const &B, torch::Tensor &C) {
+    std::cout << "\nmatMul\n" << std::endl; //Test
     constexpr uint16_t HALF_ZERO = 0;      // 0.0 in __half format
     constexpr uint16_t HALF_ONE = 0x3C00;  // 1.0 in __half format
     assert(A.dtype() == torch::kF16 && B.dtype() == torch::kF16 && C.dtype() == torch::kF16);
