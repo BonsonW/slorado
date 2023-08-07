73225f72be5593bdf446c6a7dea57295d4293f96
AchinthaSandakelum <achinthasandakelum45@gmail.com>
2023-08-07 13:30:43 +0530

    Timestamps updated v6

--- src/basecaller_main.cpp
+++ src/basecaller_main.cpp
@@ -330,12 +330,12 @@ int basecaller_main(int argc, char* argv[]) {
 
             fprintf(stderr, "\n[%s]                 - CudaCaller time: %.3f sec",__func__, CudaCallerT);
             
-            fprintf(stderr, "\n[%s]                     - CudaCaller1 time: %.3f sec",__func__, CudaCallerT1);
-            fprintf(stderr, "\n[%s]                     - CudaCaller2 time: %.3f sec",__func__, CudaCallerT2);
-            fprintf(stderr, "\n[%s]                     - CudaCaller3 time: %.3f sec",__func__, CudaCallerT3);
+            // fprintf(stderr, "\n[%s]                     - CudaCaller1 time: %.3f sec",__func__, CudaCallerT1);
+            // fprintf(stderr, "\n[%s]                     - CudaCaller2 time: %.3f sec",__func__, CudaCallerT2);
+            // fprintf(stderr, "\n[%s]                     - CudaCaller3 time: %.3f sec",__func__, CudaCallerT3);
             fprintf(stderr, "\n[%s]                     - CudaCaller4 time: %.3f sec",__func__, CudaCallerT4);
             fprintf(stderr, "\n[%s]                     - CudaCaller5 time: %.3f sec",__func__, CudaCallerT5);
-            fprintf(stderr, "\n[%s]                     - CudaCaller6 time: %.3f sec",__func__, CudaCallerT6);
+            // fprintf(stderr, "\n[%s]                     - CudaCaller6 time: %.3f sec",__func__, CudaCallerT6);
 
 
             fprintf(stderr, "\n[%s]                 - ~CudaCallerT time: %.3f sec",__func__, NCudaCallerT);
