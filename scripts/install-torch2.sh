a99a2dd0c2f8ae5209d4ea2cd0d9be413c02cf43
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2023-10-27 13:35:47 +1100

    cpu torch

--- scripts/install-torch2.sh
+++ scripts/install-torch2.sh
@@ -4,12 +4,24 @@
 # Copyright (c) 2019 Hasindu Gamaarachchi (hasindu@unsw.edu.au)
 # Copyright (c) 2023 Bonson Wong (bonson.ym@gmail.com)
 
+DEV=cuda
+test -z $1 || DEV=$1
+
 set -e
 set -x
 test -e torch.zip && rm torch.zip
 test -d torch && rm -r torch
 mkdir thirdparty/torch
-wget "https://download.pytorch.org/libtorch/cu118/libtorch-shared-with-deps-2.0.0%2Bcu118.zip" -O torch.zip
+
+if [ ${DEV} = cuda ]; then
+    LINK="https://download.pytorch.org/libtorch/cu118/libtorch-shared-with-deps-2.0.0%2Bcu118.zip"
+elif [ ${DEV} = cpu ]; then
+    LINK="https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-2.0.0%2Bcpu.zip"
+elif [ ${DEV} = rocm ]; then
+    LINK="https://download.pytorch.org/libtorch/rocm5.1.1/libtorch-shared-with-deps-2.0.0%2Brocm5.1.1.zip"
+fi
+
+wget ${LINK} -O torch.zip
 unzip torch.zip -d thirdparty/torch/
 rm torch.zip
 
