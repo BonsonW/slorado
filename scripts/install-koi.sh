e1669dba8cc43d5781938577746192693ee4e921
Bonson Wong <boson.ym@gmail.com>
2024-05-06 14:02:05 +1000

    update koi and koi install script

--- scripts/install-koi.sh
+++ scripts/install-koi.sh
@@ -1,9 +1,16 @@
 set -e
 
+KOI_VERSION=0.4.5
+CMAKE_SYSTEM_NAME=Linux
+CMAKE_SYSTEM_PROCESSOR=x86_64
+KOI_CUDA=11.8
+
+KOI_DIR=libkoi-${KOI_VERSION}-${CMAKE_SYSTEM_NAME}-${CMAKE_SYSTEM_PROCESSOR}-cuda-${KOI_CUDA}
+
 test -d koi && rm -r koi
 test -e koi.tar.gz && rm  koi.tar.gz
 test -d thirdparty/koi_lib && rm -r thirdparty/koi_lib
-wget https://cdn.oxfordnanoportal.com/software/analysis/libkoi-0.3.5-Linux-x86_64-cuda-11.8.tar.gz -O koi.tar.gz
+wget https://cdn.oxfordnanoportal.com/software/analysis/${KOI_DIR}.tar.gz -O koi.tar.gz
 mkdir koi
 mkdir thirdparty/koi_lib
 tar xf koi.tar.gz -C koi/
--- thirdparty/koi_lib/include/koi.h
+++ thirdparty/koi_lib/include/koi.h
@@ -1,52 +1,82 @@
-struct ChunkDesc {
-    unsigned int startRowIn;
-    unsigned int numRowsIn;
-    unsigned int startRowOut;
-    int padding_;
-};
+#pragma once
 
 enum KoiTypeId { KOI_I8 = 0, KOI_F16 };
+enum KoiActivation { KOI_IDENTITY = 0, KOI_SWISH, KOI_SWISH_CLAMP, KOI_TANH, KOI_TANH_X5 };
 
-int host_run_lstm_reverse96(void *chunks,
-                            void *outvW,
-                            void *lW,
-                            void *b,
-                            void *out,
-                            int num_chunks);
+enum KoiResult {
+    KOI_SUCCESS = 0,
+    KOI_NOT_SUPPORTED,  // Operation with given args is not supported on current device/platform
+    KOI_INVALID_VALUE,  // Operation with given args is not supported at all
+    KOI_INTERNAL_ERROR  // Any other error
+};
 
-int host_run_lstm_fwd96(void *chunks, void *outvW, void *lW, void *b, void *out, int num_chunks);
+/// host_linear
+///
+/// Perform
+/// - matrix multiplication (either [int8 * int8 -> int32] or [half * half -> half]),
+/// - followed by conversion to float,
+/// - pointwise application of scale vector (stored in half format)
+/// - pointwise application of bias vector (stored in half format)
+/// - application of an activation function,
+/// - and conversion to output type (cast to half if output type is KOI_F16, or mapping the float
+///   range [-1.0, 1.0] to [-127, 127] if output type is KOI_I8)
+///
+/// ```
+/// auto mm = convert<Accum_T, float>(matmul<In_T, Accum_T>(in, weights)) / weight_scale;
+/// out = convert<float, out_t>(activation(mm + bias))
+/// ```
+///
+/// \param stream cudaStream_t
+/// \param type_id_in Dtype of both `in` and `weights`. Either KOI_I8 or KOI_F16.
+/// \param activation Activation function to be applied after scale and bias
+/// \param type_id_out Dtype of `out`
+/// \param size0 `in.size(0)`
+/// \param size1 `in.size(1)`
+/// \param C_in layer input size (`in.size(2)`)
+/// \param C_out layer output size (`out.size(2)`)
+/// \param in_stride0 `in.stride(0)`
+/// \param in_stride1 `in.stride(1)`
+/// \param in `.data_ptr()` of tensor of size [`size0`, `size1`, `C_in`], strides
+///         [`in_stride0`, `in_stride1`, 1], dtype matches `type_id_in`
+/// \param weights `.data_ptr()` of tensor of size [`C_out`, `C_in`], contiguous, (i.e. strides are
+///         [`C_in`, 1]), dtype matches `type_id_in`
+/// \param out `data_ptr()` of tensor of size [`size0`, `size1`, 'C_out`], contiguous, (i.e. strides
+///         are [`size1 * C_out`, `C_out`, 1]), dtype matches `type_id_out`
+/// \param weight_scale `.data_ptr()` of tensor of size [`C_out`], stride [1], dtype float16.
+///         May be nullptr (equivalent to vector of all ones)
+/// \param bias `.data_ptr() of tensor of size [`C_out`], stride [1], dtype float16.
+///         May be nullptr (equivalent to vector of all zeroes)
+/// \return KOI_SUCCESS on success, other KoiResult values otherwise
+int host_linear(void *stream,
+                enum KoiTypeId type_id_in,
+                enum KoiActivation activation,
+                enum KoiTypeId type_id_out,
+                int size0,
+                int size1,
+                int C_in,
+                int C_out,
+                int in_stride0,
+                int in_stride1,
+                int out_stride0,
+                int out_stride1,
+                void *in,
+                void *weights,
+                void *out,
+                void *weight_scale,
+                void *bias);
 
-int host_run_lstm_reverse_quantized96(void *chunks,
-                                      void *outvW,
-                                      void *lW,
-                                      void *b,
-                                      void *quantization_scale,
-                                      void *out,
-                                      int num_chunks);
+int host_cublas_gemm_f16(void *cublas_h, int m, int n, int k, int flags, void *A, void *B, void *C);
 
-int host_run_lstm_fwd_quantized96(void *chunks,
-                                  void *outvW,
-                                  void *lW,
-                                  void *b,
-                                  void *quantization_scale,
-                                  void *out,
-                                  int num_chunks);
-
-int host_run_lstm_reverse_quantized128(void *chunks,
-                                       void *outvW,
-                                       void *lW,
-                                       void *b,
-                                       void *quantization_scale,
-                                       void *out,
-                                       int num_chunks);
-
-int host_run_lstm_fwd_quantized128(void *chunks,
-                                   void *outvW,
-                                   void *lW,
-                                   void *b,
-                                   void *quantization_scale,
-                                   void *out,
-                                   int num_chunks);
+int host_small_lstm(void *stream,
+                    int num_chunks,
+                    int chunk_size,
+                    int layer_size,
+                    int direction,
+                    void *outvW,
+                    void *lW,
+                    void *b,
+                    void *quantization_scale,
+                    void *out);
 
 void host_lstm_step_f16(void *stream,
                         int batch_size,
@@ -73,112 +103,74 @@ void host_cutlass_lstm(void *stream,
                        int interleave,
                        int brf_state);
 
-void host_convert(void *stream,
-                  void *in,
-                  int in_stride0,
-                  int in_stride1,
-                  int in_stride2,
-                  enum KoiTypeId in_type,
-                  void *out,
-                  int out_stride0,
-                  int out_stride1,
-                  int out_stride2,
-                  enum KoiTypeId out_type,
-                  int size0,
-                  int size1,
-                  int size2);
+int host_convert(void *stream,
+                 void *in,
+                 int in_stride0,
+                 int in_stride1,
+                 int in_stride2,
+                 enum KoiTypeId in_type,
+                 void *out,
+                 int out_stride0,
+                 int out_stride1,
+                 int out_stride2,
+                 enum KoiTypeId out_type,
+                 int size0,
+                 int size1,
+                 int size2);
 
-void host_convert_inplace(void *stream,
-                          void *in_out,
-                          enum KoiTypeId in_type,
-                          enum KoiTypeId out_type,
-                          int larger_stride0,
-                          int smaller_offset1,
-                          int size0,
-                          int size1);
+int host_bias_activation_f16_inplace(void *stream,
+                                     int TN,
+                                     int C,
+                                     int stride,
+                                     void *in_out,
+                                     void *bias,
+                                     enum KoiActivation activation);
 
-void host_bias_tanh_scale_f16(void *stream,
-                              int num_rows,
-                              int layer_size,
-                              float scale_factor,
-                              void *in_out,
-                              void *bias);
+int host_bias_activation_interleave_convert(void *stream,
+                                            int T,
+                                            int N,
+                                            int C,
+                                            void *in,
+                                            int in_interleave,
+                                            enum KoiTypeId in_type_id,
+                                            void *out,
+                                            int out_interleave,
+                                            enum KoiTypeId out_type_id,
+                                            void *bias,
+                                            enum KoiActivation activation);
 
-void host_bias_swish_f16_clamp(void *stream,
-                               int size0,
-                               int size1,
-                               int stride0,
-                               void *in_out,
-                               void *bias,
-                               float max_value);
+int host_window_ntwc_f16(void *stream,
+                         int N,
+                         int T_in,
+                         int C,
+                         int W,
+                         int conv_stride,
+                         int N_out_stride,
+                         int T_out_stride,
+                         void *in_buf,
+                         void *out_buf);
 
-void host_bias_swish_f16_i8(void *stream,
-                            int size0,
-                            int size1,
-                            int in_stride0,
-                            int out_stride0,
-                            void *in,
-                            void *out,
-                            void *bias,
-                            float scale,
-                            float zero_offset);
+int host_convolution_f16(void *stream,
+                         int N,
+                         int C_in,
+                         int C_out,
+                         int T_in,
+                         int window,
+                         int stride,
+                         int padding,
+                         int out_N_stride,
+                         void *in_buf,
+                         void *out_buf,
+                         void *weights,
+                         void *bias,
+                         enum KoiActivation activation);
 
-void host_bias_swish_f16_i8_inplace(void *stream,
-                                    int size0,
-                                    int size1,
-                                    int out_offset,
-                                    void *in_out,
-                                    void *bias,
-                                    float scale,
-                                    float zero_offset);
-
-void host_window_ntcw_f16(void *stream,
-                          int N_in_stride,
-                          int T_in_stride,
-                          int C_in_stride,
-                          int N,
-                          int T_in,
-                          int C,
-                          int W,
-                          int conv_stride,
-                          int N_out_stride,
-                          int T_out_stride,
-                          int C_out_stride,
-                          int W_out_stride,
-                          void *in_buf,
-                          void *out_buf);
-
-int host_convolution_swish_f16(void *stream,
-                               int N,
-                               int C_in,
-                               int C_out,
-                               int T_in,
-                               int window,
-                               int stride,
-                               int padding,
-                               void *in_buf,
-                               void *out_buf,
-                               void *weights,
-                               void *bias,
-                               float clamp_max);
-
-void host_transpose_f16(void *stream,
-                        void *in,
-                        int size0,
-                        int size1,
-                        int size2,
-                        int in_stride0,
-                        int in_stride1,
-                        int in_stride2,
-                        int out_stride0,
-                        int out_stride1,
-                        int out_stride2,
-                        void *out);
-
-int host_back_guide_step(void *chunks,
+int host_back_guide_step(void *stream,
+                         void *chunks,
                          void *chunk_results,
                          int num_chunks,
                          void *post,
+                         float post_clamp,
                          int post_stride,
                          void *aux_buffer,
                          void *path,
@@ -192,10 +184,12 @@ int host_back_guide_step(void *chunks,
                          float beam_cut,
                          float fixed_stay_score);
 
-int host_beam_search_step(void *chunks,
+int host_beam_search_step(void *stream,
+                          void *chunks,
                           void *chunk_results,
                           int num_chunks,
                           void *post,
+                          float post_clamp,
                           int post_stride,
                           void *aux_buffer,
                           void *path,
@@ -209,10 +203,12 @@ int host_beam_search_step(void *chunks,
                           float beam_cut,
                           float fixed_stay_score);
 
-int host_compute_posts_step(void *chunks,
+int host_compute_posts_step(void *stream,
+                            void *chunks,
                             void *chunk_results,
                             int num_chunks,
                             void *post,
+                            float post_clamp,
                             int post_stride,
                             void *aux_buffer,
                             void *path,
@@ -226,10 +222,12 @@ int host_compute_posts_step(void *chunks,
                             float beam_cut,
                             float fixed_stay_score);
 
-int host_run_decode(void *chunks,
+int host_run_decode(void *stream,
+                    void *chunks,
                     void *chunk_results,
                     int num_chunks,
                     void *post,
+                    float post_clamp,
                     int post_stride,
                     void *aux_buffer,
                     void *path,
