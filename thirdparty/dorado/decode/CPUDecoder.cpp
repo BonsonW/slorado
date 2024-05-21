2231a1ecaea27ed6461b42bb6678679c7b476c8e
Bonson Wong <boson.ym@gmail.com>
2024-05-21 19:57:19 +1000

    tensor stuff separate thread

--- thirdparty/dorado/decode/CPUDecoder.cpp
+++ thirdparty/dorado/decode/CPUDecoder.cpp
@@ -90,32 +90,60 @@ torch::Tensor backward_scores(const torch::Tensor& scores, const float fixed_sta
 typedef struct {
     const DecoderOptions *options;
     const torch::Tensor *scores_cpu;
+    std::vector<torch::Tensor> *scores;
+    std::vector<torch::Tensor> *bwd;
+    std::vector<torch::Tensor> *posts;
+    int32_t tid;
+    int32_t start;
+    int32_t end;
+} scores_guide_thread_arg_t;
+
+void* pthread_scores_guide_single(void* voidargs) {
+    scores_guide_thread_arg_t* args = (scores_guide_thread_arg_t*)voidargs;
+    const DecoderOptions *options = args->options;
+
+    using Slice = torch::indexing::Slice;
+    // we slice on the second index because we want to split the batch up not the chunk up
+    // (first index is timestep in chunk, second index is chunk in batch)
+    auto t_scores = args->scores_cpu->index({Slice(), Slice(args->start, args->end)});
+
+    torch::Tensor t_fwd = forward_scores(t_scores, options->blank_score);
+    torch::Tensor t_bwd = backward_scores(t_scores, options->blank_score);
+
+    torch::Tensor t_posts = torch::softmax(t_fwd + t_bwd, -1);
+
+    t_scores = t_scores.transpose(0, 1);
+    t_bwd = t_bwd.transpose(0, 1).contiguous();
+    t_posts = t_posts.transpose(0, 1).contiguous();
+
+    int tid = args->tid;
+    (*args->scores)[tid] = t_scores;
+    (*args->bwd)[tid] = t_bwd;
+    (*args->posts)[tid] = t_posts;
+
+    pthread_exit(0);
+}
+
+typedef struct {
+    const DecoderOptions *options;
+    std::vector<torch::Tensor> *scores;
+    std::vector<torch::Tensor> *bwd;
+    std::vector<torch::Tensor> *posts;
     std::vector<DecodedChunk> *chunk_results;
+    int32_t tid;
     int32_t start;
     int32_t end;
 } decode_thread_arg_t;
 
 void* pthread_single_beam_search(void* voidargs) {
-
     decode_thread_arg_t* args = (decode_thread_arg_t*)voidargs;
     const DecoderOptions *options = args->options;
 
-    using Slice = torch::indexing::Slice;
-    auto t_scores = args->scores_cpu->index({Slice(), Slice(args->start, args->end)});
-
-    torch::Tensor fwd = forward_scores(t_scores, options->blank_score);
-    torch::Tensor bwd = backward_scores(t_scores, options->blank_score);
-
-    torch::Tensor posts = torch::softmax(fwd + bwd, -1);
-
-    t_scores = t_scores.transpose(0, 1);
-    bwd = bwd.transpose(0, 1).contiguous();
-    posts = posts.transpose(0, 1).contiguous();
-
     int i = 0;
+    int tid = args->tid;
     for (int c = args->start; c < args->end; c++, i++) {
         auto decode_result = beam_search_decode(
-                t_scores[i], bwd[i], posts[i], options->beam_width, options->beam_cut,
+                args->scores[tid][i], args->bwd[tid][i], args->posts[tid][i], options->beam_width, options->beam_cut,
                 options->blank_score, options->q_shift, options->q_scale,
                 options->temperature, 1.0f);
         (*args->chunk_results)[c] = DecodedChunk{
@@ -128,27 +156,57 @@ void* pthread_single_beam_search(void* voidargs) {
     pthread_exit(0);
 }
 
-std::vector<DecodedChunk> beam_search_cpu(const torch::Tensor& scores,
+std::vector<DecodedChunk> beam_search_cpu(const torch::Tensor& _scores,
                                                   const int num_chunks,
                                                   const DecoderOptions& options,
                                                   std::string &device) {
-    const auto scores_cpu = scores.to(torch::kCPU).to(CPUDecoder::dtype).transpose(0, 1);
-    int num_threads = std::min(num_chunks, 4);
+    const auto scores_cpu = _scores.to(torch::kCPU).to(CPUDecoder::dtype).transpose(0, 1);
+    int num_threads = std::min(num_chunks, 1);
     int chunks_per_thread = num_chunks / num_threads;
     int num_threads_with_one_more_chunk = num_chunks % num_threads;
 
+    std::vector<torch::Tensor> scores(num_threads);
+    std::vector<torch::Tensor> bwd(num_threads);
+    std::vector<torch::Tensor> posts(num_threads);
+
     std::vector<DecodedChunk> chunk_results(num_chunks);
 
-    //create threads
+    // create threads
     pthread_t tids[num_threads];
+    scores_guide_thread_arg_t scores_pt_args[num_threads];
     decode_thread_arg_t pt_args[num_threads];
     int32_t t, ret;
 
-    //set the data structures
+    // prep tensors
     for (t = 0; t < num_threads; t++) {
+        scores_pt_args[t].tid = t;
+        scores_pt_args[t].scores = &scores;
+        scores_pt_args[t].bwd = &bwd;
+        scores_pt_args[t].posts = &posts;
+        scores_pt_args[t].start = t * chunks_per_thread + std::min(t, num_threads_with_one_more_chunk);
+        scores_pt_args[t].end = pt_args[t].start + chunks_per_thread + int(t < num_threads_with_one_more_chunk);
+        scores_pt_args[t].scores_cpu = &scores_cpu;
+        scores_pt_args[t].options = &options;
+    }
+    
+    for (t = 0; t < num_threads; t++) {
+        ret = pthread_create(&tids[t], NULL, pthread_scores_guide_single, (void*)(&scores_pt_args[t]));
+        NEG_CHK(ret);
+    }
+
+    for (t = 0; t < num_threads; t++) {
+        ret = pthread_join(tids[t], NULL);
+        NEG_CHK(ret);
+    }
+
+    // decode
+    for (t = 0; t < num_threads; t++) {
+        pt_args[t].tid = t;
+        pt_args[t].scores = &scores;
+        pt_args[t].bwd = &bwd;
+        pt_args[t].posts = &posts;
         pt_args[t].start = t * chunks_per_thread + std::min(t, num_threads_with_one_more_chunk);
         pt_args[t].end = pt_args[t].start + chunks_per_thread + int(t < num_threads_with_one_more_chunk);
-        pt_args[t].scores_cpu = &scores_cpu;
         pt_args[t].chunk_results = &chunk_results;
         pt_args[t].options = &options;
     }
