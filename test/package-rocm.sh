5f5b9252ae7a8067db57558490075f0e45b62f9e
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-11-06 00:02:23 +1100

    copy models

--- test/package-rocm.sh
+++ test/package-rocm.sh
@@ -8,14 +8,18 @@ die() {
 VERSION=`git describe --tags`
 [ -z "$VERSION" ] && VERSION=`git rev-parse --short HEAD`
 
-rm -rf slorado-$VERSION slorado-$VERSION.tar.gz
+rm -rf slorado-$VERSION slorado-*.tar.gz models/
+test/download-models.sh || die "Download models failed"
+
 mkdir -p slorado-$VERSION
-mkdir -p slorado-$VERSION/bin slorado-$VERSION/lib
+mkdir -p slorado-$VERSION/bin slorado-$VERSION/lib slorado-$VERSION/share
 make clean
 make rocm=1 -j  || die "Build failed"
 
+cp -r thirdparty/torch/libtorch/* slorado-$VERSION/|| die "Copy failed"
 mv  ./slorado slorado-$VERSION/bin/ || die "Copy failed"
-cp -r thirdparty/torch/libtorch/lib/lib*.so* slorado-$VERSION/lib/ || die "Copy failed"
+cp -r models/ slorado-$VERSION/ || die "Copy failed"
+
 ./slorado-$VERSION/bin/slorado --version || die "Test failed"
 tar zcf slorado-$VERSION.tar.gz slorado-$VERSION || die "Tar failed"
 rm -rf slorado-$VERSION
