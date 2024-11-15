6f9dbb58b18854a53bc2c4781c6a24368d878094
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-11-15 22:45:09 +1100

    hiparch

--- test/package-rocm.sh
+++ test/package-rocm.sh
@@ -11,11 +11,12 @@ VERSION=`git describe --tags`
 rm -rf slorado-$VERSION slorado-*.tar.gz models/
 scripts/download-models.sh || die "Download models failed"
 
+test -d thirdparty/torch/libtorch || scripts/install-torch2.sh rocm || die "Install torch failed"
+
 mkdir -p slorado-$VERSION
 mkdir -p slorado-$VERSION/bin slorado-$VERSION/lib slorado-$VERSION/share
 make clean
-make rocm=1 -j  || die "Build failed"
-
+make rocm=1 -j HIP_ARCH="--offload-arch=gfx1030 --offload-arch=gfx1100 --offload-arch=gfx900 --offload-arch=gfx906 --offload-arch=gfx908 --offload-arch=gfx90a" || die "Build failed"
 cp -r thirdparty/torch/libtorch/* slorado-$VERSION/|| die "Copy failed"
 mv  ./slorado slorado-$VERSION/bin/ || die "Copy failed"
 cp -r models/ slorado-$VERSION/ || die "Copy failed"
