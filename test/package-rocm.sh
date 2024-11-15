d40a98ec684032249daa94150ee04f2d4bba252a
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-11-15 23:20:25 +1100

    hiparch

--- test/package-rocm.sh
+++ test/package-rocm.sh
@@ -17,6 +17,8 @@ mkdir -p slorado-$VERSION
 mkdir -p slorado-$VERSION/bin slorado-$VERSION/lib slorado-$VERSION/share
 make clean
 make rocm=1 -j HIP_ARCH='"--offload-arch=gfx1030 --offload-arch=gfx1100 --offload-arch=gfx900 --offload-arch=gfx906 --offload-arch=gfx908 --offload-arch=gfx90a"' || die "Build failed"
+rm -f thirdparty/torch/libtorch/lib/libamdhip64.so.5
+ln -s libamdhip64.so thirdparty/torch/libtorch/lib/libamdhip64.so.5 || die "Link failed"
 cp -r thirdparty/torch/libtorch/* slorado-$VERSION/|| die "Copy failed"
 mv  ./slorado slorado-$VERSION/bin/ || die "Copy failed"
 cp -r models/ slorado-$VERSION/ || die "Copy failed"
