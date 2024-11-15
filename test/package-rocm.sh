e7c7e549c9153c6b9d8d7350150dc34e463ec107
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-11-15 23:44:39 +1100

    update package script

--- test/package-rocm.sh
+++ test/package-rocm.sh
@@ -15,6 +15,8 @@ test -d thirdparty/torch/libtorch || scripts/install-torch2.sh rocm || die "Inst
 
 mkdir -p slorado-$VERSION
 mkdir -p slorado-$VERSION/bin slorado-$VERSION/lib slorado-$VERSION/share
+
+# roc-obj-ls libtorch_hip.so  | awk '{print $2}' | sort -u
 make clean
 make rocm=1 -j HIP_ARCH='"--offload-arch=gfx1030 --offload-arch=gfx1100 --offload-arch=gfx900 --offload-arch=gfx906 --offload-arch=gfx908 --offload-arch=gfx90a"' || die "Build failed"
 rm -f thirdparty/torch/libtorch/lib/libamdhip64.so.5
@@ -22,6 +24,12 @@ ln -s libamdhip64.so thirdparty/torch/libtorch/lib/libamdhip64.so.5 || die "Link
 cp -r thirdparty/torch/libtorch/* slorado-$VERSION/|| die "Copy failed"
 mv  ./slorado slorado-$VERSION/bin/ || die "Copy failed"
 cp -r models/ slorado-$VERSION/ || die "Copy failed"
+
+# clean up
+mv slorado-$VERSION/build-hash slorado-$VERSION/build-version  slorado-$VERSION/share/ || die "mv failed"
+rm -r slorado-$VERSION/include
+rm -r slorado-$VERSION/share/cmake
+rm -r slorado-$VERSION/lib/*.a
 
 ./slorado-$VERSION/bin/slorado --version || die "Test failed"
 tar zcf slorado-$VERSION.tar.gz slorado-$VERSION || die "Tar failed"
