79ea2ea87e803db20d0870c42c872b01c37327f5
Bonson Wong <boson.ym@gmail.com>
2024-11-20 08:33:01 +1100

    fix writer error check

--- src/writer.cpp
+++ src/writer.cpp
@@ -38,8 +38,10 @@ SOFTWARE.
 
 void write_to_file(FILE *out, char *sequence, char *qstring, char *read_id, bool emit_fastq) {
     if (emit_fastq) {
-        if (fprintf(out, "@%s\n%s\n+\n%s\n", read_id, sequence, qstring) == EOF) {
-            ERROR("%s", "error writing to file");
+        int expected_len_bytes = strlen(read_id) + strlen(sequence) + strlen(qstring) + 6;
+        int ret = fprintf(out, "@%s\n%s\n+\n%s\n", read_id, sequence, qstring);
+        if (ret != expected_len_bytes) {
+            ERROR("error writing fastq: %s", strerror(ret));
             exit(EXIT_FAILURE);
         }
     } else {
--- test/gtgpu_run.sh
+++ test/gtgpu_run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-DEVICE=cuda:1
-MODEL=hac
-BATCHSIZE=700
+DEVICE=cuda:3
+MODEL=fast
+BATCHSIZE=1000
 CHUNKSIZE=10000
 THREADS=40
 
 # BLOW5=/data/slow5-testdata/hg2_prom_lsk114_5khz_subsample/PGXXXX230339_reads_500k.blow5
 BLOW5=/data/slow5-testdata/hg2_prom_lsk114_5khz_subsubsample/PGXXXX230339_reads_20k.blow5
-# BLOW5=/data/bonwon/slorado/test/one_5khz.blow5
+# BLOW5=/data/bonwon/slorado/test/4khz_r10/one_read.blow5
 # BLOW5=test/4khz_r10/1k_reads.blow5
 
 /usr/bin/time  --verbose ./slorado basecaller -t $THREADS -c $CHUNKSIZE -C $BATCHSIZE -B500M -x $DEVICE --verbose 5 /install/dorado-0.3.4/models/dna_r10.4.1_e8.2_400bps_${MODEL}@v4.2.0 $BLOW5 > reads.fastq
