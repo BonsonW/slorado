56cf090c5f3043858e4fc7c0e62279db88f29f77
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-11-22 16:12:15 +1100

    zstd local

--- test/package-cuda.sh
+++ test/package-cuda.sh
@@ -5,7 +5,7 @@ die() {
     exit 1
 }
 
-scl enable devtoolset-8 bash
+source /opt/rh/devtoolset-8/enable || die "Enable devtoolset-8 failed"
 
 git submodule update || die "Update failed"
 
@@ -14,6 +14,7 @@ VERSION=`git describe --tags`
 
 rm -rf slorado-$VERSION slorado-*.tar.gz models/
 scripts/download-models.sh || die "Download models failed"
+scripts/install-zstd.sh || die "Install zstd failed"
 
 test -d thirdparty/torch/libtorch || scripts/install-torch2.sh cuda || die "Install torch failed"
 
@@ -22,7 +23,7 @@ mkdir -p slorado-$VERSION/bin slorado-$VERSION/lib slorado-$VERSION/share
 
 # roc-obj-ls libtorch_hip.so  | awk '{print $2}' | sort -u
 make clean
-make cuda=1 || die "Build failed"
+make cuda=1 zstd_local=../zstd/lib || die "Build failed"
 cp -r thirdparty/torch/libtorch/* slorado-$VERSION/|| die "Copy failed"
 mv  ./slorado slorado-$VERSION/bin/ || die "Copy failed"
 cp -r models/ slorado-$VERSION/ || die "Copy failed"
@@ -32,6 +33,7 @@ mv slorado-$VERSION/build-hash slorado-$VERSION/build-version  slorado-$VERSION/
 rm -r slorado-$VERSION/include
 rm -r slorado-$VERSION/share/cmake
 rm -r slorado-$VERSION/lib/*.a
+rm slorado-$VERSION/lib/
 
 ./slorado-$VERSION/bin/slorado --version || die "Test failed"
 tar zcf slorado-$VERSION-x86_64-cuda-linux-binaries.tar.gz slorado-$VERSION || die "Tar failed"
