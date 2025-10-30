499b1cb26875fd5c6d77029714c8c89f83cf97d3
bonwon <bonson.ym@gmail.com>
2025-10-30 16:02:00 +1100

    fix last commit

--- scripts/calculate_basecalling_accuracy.sh
+++ scripts/calculate_basecalling_accuracy.sh
@@ -22,4 +22,4 @@ FASTQ_FILE=$2 #path to basecalled fastq file
 
 echo "identity scores:"
 echo -e "mean\tstdev\tq1\tmedian\tq3\tn"
-${MINIMAP2} -cx map-ont ${REFERENC_GENOME} -t8 --secondary=no ${FASTQ_FILE} | awk '{print $10/$11}' | datamash mean 1 sstdev 1 q1 1 median 1 q3 1 n 1 || die "Error in identity calculation"
+${MINIMAP2} -cx map-ont ${REFERENC_GENOME} -t8 --secondary=no ${FASTQ_FILE} | awk '{print $10/$11}' | datamash mean 1 sstdev 1 q1 1 median 1 q3 1 count 1 || die "Error in identity calculation"
