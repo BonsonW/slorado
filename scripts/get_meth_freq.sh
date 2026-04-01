cb8e7e1544d9917f59bd68019926675c8c4669b5
BonsonW <bonson.ym@gmail.com>
2026-04-02 10:19:00 +1100

    add scripts for meth freq tests

---
+++ scripts/get_meth_freq.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+die() {
+    echo "$@" >&2
+    exit 1
+}
+
+# map ref genome
+map() {
+	bam=$1
+	genome=$2
+
+	samtools fastq -@64 -TMM,ML "$bam" | minimap2 -t64 -x map-ont -a -y -Y --secondary=no "$genome" - | samtools sort -@64 -
+}
+
+if [ $# -lt 3 ]; then
+	die "Usage: $0 <reference genome> <fastq/sam/bam file> <output file>.mm.tsv|<output file>.mm.bedmethyl"
+fi
+
+SAMTOOLS=samtools # path to samtools
+MINIMOD=minimod # path to minimod
+
+GENOME=$1 # path to reference genome
+BAM=$2 # path to unmapped sam/bam output
+OUT=$3 # output file path
+
+BAM_MAP=mapped.sam # path to mapped bam output
+
+map "$BAM" "$GENOME" > "$BAM_MAP" | die "mapping failed"
+
+$SAMTOOLS index "$BAM_MAP" | die "indexing failed"
+
+rm $BAM_MAP
+
+# get meth freq
+if [[ "$OUT" == *.mm.bedmethyl ]]; then
+	$MINIMOD freq "$GENOME" "$BAM_MAP" -b > "$OUT" || die "mod freq failed"
+elif [[ "$OUT" == *.mm.tsv ]]; then
+	$MINIMOD freq "$GENOME" "$BAM_MAP" > "$OUT" || die "mod freq failed"
+else
+	die "unsupported output suffix: '$OUT' (use .mm.tsv or .mm.bedmethyl)"
+fi
