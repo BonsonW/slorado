99b23cf0848fa2782499dacb90807aded8e11e31
BonsonW <bonson.ym@gmail.com>
2026-08-22 08:53:03 +1000

    update acc script

--- scripts/calculate_basecalling_accuracy.sh
+++ scripts/calculate_basecalling_accuracy.sh
@@ -17,9 +17,9 @@ fi
 ${MINIMAP2} --version &> /dev/null || { echo -e $RED"minimap2 not found! Either put minimap2 under path or set MINIMAP2 variable, e.g.,export MINIMAP2=/path/to/minimap2"$NORMAL; exit 1;}
 ${DATAMASH} --version &> /dev/null || { echo -e $RED"datamash not found! Either put datamash under path or set DATAMASH variable, e.g.,export DATAMASH=/path/to/datamash, or if not installed: \`apt-get install datamash\`"$NORMAL; exit 1;}
 
-REFERENC_GENOME=$1 #path to reference genome
-FASTQ_FILE=$2 #path to basecalled fastq file
+REF=$1 #path to reference genome
+FASTQ=$2 #path to basecalled fastq file
 
-echo "identity scores:"
-echo -e "mean\tstdev\tq1\tmedian\tq3\tn"
-${MINIMAP2} -cx map-ont ${REFERENC_GENOME} -t${NTHREADS} --secondary=no ${FASTQ_FILE} | awk '{print $10/$11}' | ${DATAMASH} mean 1 sstdev 1 q1 1 median 1 q3 1 count 1 || die "Error in identity calculation"
+OUTPUT=$(${MINIMAP2} -cx map-ont ${REF} -t${NTHREADS} --secondary=no ${FASTQ} | awk '{print $10/$11}' | ${DATAMASH} mean 1 sstdev 1 q1 1 median 1 q3 1 count 1 || die "Error in identity calculation")
+echo -en "sample\tmean\tsstdev\tq1\tmedian\tq3\tn\n"
+echo -en "${FASTQ}\t$OUTPUT\n"
